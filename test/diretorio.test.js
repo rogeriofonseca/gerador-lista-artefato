e0cc579f6b9cce79d140a42e066f0a1384dca14c
Diego Medeiros <c1282036@bb.com.br>
2020-02-27 16:46:22 -0300

    Update

--- test/diretorio.test.js
+++ test/diretorio.test.js
@@ -43,7 +43,7 @@ describe('test gerais', () => {
 
     it('teste listar sub-diretorios segundo nivel', async () => {
 
-        const caminho = '/tmp' + path.sep + appName + path.sep + 'foo' 
+        const caminho = '/tmp' + path.sep + appName + path.sep + 'foo'
 
         const gitFoo = await new GeradorTestUtil(caminho, 'foo', autor)
         const gitBar = await new GeradorTestUtil(caminho, 'bar', autor)
@@ -53,6 +53,25 @@ describe('test gerais', () => {
         const lista = await diretorio.listarDiretorios()
 
         expect(lista[0]).toBe('/tmp/gerador-lista-artefato-qas/foo/bar')
+        expect(lista[1]).toBe('/tmp/gerador-lista-artefato-qas/foo/foo')
+
+        gitFoo.removerDiretorioTest()
+        gitBar.removerDiretorioTest()
+    })
+
+    it('teste listar sub-diretorios primeiro e segundo nivel', async () => {
+
+        const caminhoPrimeiroNivel = '/tmp' + path.sep + appName
+        const caminhoSegundoNivel = '/tmp' + path.sep + appName + path.sep + 'foo'
+
+        const gitFoo = await new GeradorTestUtil(caminhoPrimeiroNivel, 'bar', autor)
+        const gitBar = await new GeradorTestUtil(caminhoSegundoNivel, 'foo', autor)
+
+        diretorio = require('../lib/diretorio')('/tmp/gerador-lista-artefato-qas')
+
+        const lista = await diretorio.listarDiretorios()
+
+        expect(lista[0]).toBe('/tmp/gerador-lista-artefato-qas/bar')
         expect(lista[1]).toBe('/tmp/gerador-lista-artefato-qas/foo/foo')
 
         gitFoo.removerDiretorioTest()
