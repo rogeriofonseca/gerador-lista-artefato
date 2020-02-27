3f9cbdc975de7754a14552031213604bdad5d0f4
Diego Medeiros <c1282036@bb.com.br>
2020-02-27 16:33:31 -0300

    Update

--- test/diretorio.test.js
+++ test/diretorio.test.js
@@ -12,22 +12,23 @@ let diretorio, gitUtil = {}
 // jest --runInBand --verbose test/diretorio.test.js 
 describe('test gerais', () => {
 
-    beforeEach(async () => {
-
-        await new GeradorTestUtil(caminho, 'foo', autor)
-        await new GeradorTestUtil(caminho, 'bar', autor)
-    })
-
     it('teste diretorio', async () => {
+
+        const gitBar = await new GeradorTestUtil(caminho, 'bar', autor)
 
         diretorio = require('../lib/diretorio')('/tmp/gerador-lista-artefato-qas/bar')
 
         const lista = await diretorio.listarDiretorios()
 
         expect(lista[0]).toBe('/tmp/gerador-lista-artefato-qas/bar')
+
+        gitBar.removerDiretorioTest()
     })
 
     it('teste listar sub-diretorios primeiro nivel', async () => {
+
+        const gitFoo = await new GeradorTestUtil(caminho, 'foo', autor)
+        const gitBar = await new GeradorTestUtil(caminho, 'bar', autor)
 
         diretorio = require('../lib/diretorio')('/tmp/gerador-lista-artefato-qas')
 
@@ -35,10 +36,8 @@ describe('test gerais', () => {
 
         expect(lista[0]).toBe('/tmp/gerador-lista-artefato-qas/bar')
         expect(lista[1]).toBe('/tmp/gerador-lista-artefato-qas/foo')
-    })
 
-    afterAll(async () => {
-
-        gitUtil.removerDiretorioTest()
+        gitFoo.removerDiretorioTest()
+        gitBar.removerDiretorioTest()
     })
 })
