7e6c5a9473613b7abdedaac6e47af5e829aa9fb7
Diego Medeiros <c1282036@bb.com.br>
2020-02-28 11:02:26 -0300

    Update

--- test/diretorio.test.js
+++ test/diretorio.test.js
@@ -1,7 +1,8 @@
 const path = require('path')
+const fs = require('fs-extra')
 
 const autor = 'fulano'
-const caminho = require('../package.json').name
+const nomeApp = require('../package.json').name
 
 const GeradorTestUtil = require('./gerador-test-util')
 
@@ -13,7 +14,7 @@ describe('test gerais', () => {
 
     it('teste diretorio', async () => {
 
-        const gitBar = await new GeradorTestUtil(caminho, 'bar', autor)
+        const gitBar = await new GeradorTestUtil(nomeApp, 'bar', autor)
 
         diretorio = require('../lib/diretorio')('/tmp/gerador-lista-artefato-qas/bar')
 
@@ -26,8 +27,8 @@ describe('test gerais', () => {
 
     it('teste listar sub-diretorios primeiro nivel', async () => {
 
-        const gitFoo = await new GeradorTestUtil(caminho, 'foo', autor)
-        const gitBar = await new GeradorTestUtil(caminho, 'bar', autor)
+        const gitFoo = await new GeradorTestUtil(nomeApp, 'foo', autor)
+        const gitBar = await new GeradorTestUtil(nomeApp, 'bar', autor)
 
         diretorio = require('../lib/diretorio')('/tmp/gerador-lista-artefato-qas')
 
@@ -42,7 +43,7 @@ describe('test gerais', () => {
 
     it('teste listar sub-diretorios segundo nivel', async () => {
 
-        const caminhoSegundoNivel = caminho + path.sep + 'foo'
+        const caminhoSegundoNivel = nomeApp + path.sep + 'foo'
 
         const gitFoo = await new GeradorTestUtil(caminhoSegundoNivel, 'foo', autor)
         const gitBar = await new GeradorTestUtil(caminhoSegundoNivel, 'bar', autor)
@@ -60,8 +61,8 @@ describe('test gerais', () => {
 
     it('teste listar sub-diretorios primeiro e segundo nivel', async () => {
 
-        const caminhoPrimeiroNivel = caminho
-        const caminhoSegundoNivel = caminho + path.sep + 'foo'
+        const caminhoPrimeiroNivel = nomeApp
+        const caminhoSegundoNivel = nomeApp + path.sep + 'foo'
 
         const gitFoo = await new GeradorTestUtil(caminhoPrimeiroNivel, 'bar', autor)
         const gitBar = await new GeradorTestUtil(caminhoSegundoNivel, 'foo', autor)
@@ -75,5 +76,42 @@ describe('test gerais', () => {
 
         gitFoo.removerDiretorioTest()
         gitBar.removerDiretorioTest()
+    })
+
+    it('teste listar sub-diretorios até o quinto nível', async () => {
+
+        const caminhoPrimeiroNivel = nomeApp
+        const caminhoSegundoNivel = nomeApp + path.sep + 'foo'
+        const caminhoTerceiroNivel = nomeApp + path.sep + 'bar' + path.sep + 'qux'
+        const caminhoQuartoNivel = nomeApp + path.sep + 'baz' + path.sep + 'foobar' + path.sep + 'waldo'
+        const caminhoQuintoNivel = nomeApp + path.sep + 'quuz' + path.sep + 'fred' + path.sep + 'flob'
+
+        fs.mkdirsSync(caminhoPrimeiroNivel + path.sep + 'agx')
+        fs.mkdirsSync(caminhoSegundoNivel + path.sep + 'agx')
+        fs.mkdirsSync(caminhoTerceiroNivel + path.sep + 'agx')
+        fs.mkdirsSync(caminhoQuartoNivel + path.sep + 'agx')
+        fs.mkdirsSync(caminhoQuintoNivel + path.sep + 'agx')
+
+        const gitBar = await new GeradorTestUtil(caminhoPrimeiroNivel, 'bar', autor)
+        const gitFoo = await new GeradorTestUtil(caminhoSegundoNivel, 'foo', autor)
+        const gitBaz = await new GeradorTestUtil(caminhoTerceiroNivel, 'baz', autor)
+        const gitQux = await new GeradorTestUtil(caminhoQuartoNivel, 'qux', autor)
+        const gitThu = await new GeradorTestUtil(caminhoQuintoNivel, 'thu', autor)
+
+        diretorio = require('../lib/diretorio')('/tmp/gerador-lista-artefato-qas')
+
+        const lista = await diretorio.listarDiretorios()
+
+        expect(lista[0]).toBe('/tmp/gerador-lista-artefato-qas/bar')
+        expect(lista[1]).toBe('/tmp/gerador-lista-artefato-qas/bar/qux/baz')
+        expect(lista[2]).toBe('/tmp/gerador-lista-artefato-qas/baz/foobar/waldo/qux')
+        expect(lista[3]).toBe('/tmp/gerador-lista-artefato-qas/foo/foo')
+        expect(lista[4]).toBe('/tmp/gerador-lista-artefato-qas/quuz/fred/flob/thu')
+
+        gitBar.removerDiretorioTest()
+        gitFoo.removerDiretorioTest()
+        gitBaz.removerDiretorioTest()
+        gitQux.removerDiretorioTest()
+        gitThu.removerDiretorioTest()
     })
 })
